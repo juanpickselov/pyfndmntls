17c5656c3063cf9eb6783a68f8206c98fd2d49d8
Juan Pickselov <juanpickselov@sbcglobal.net>
2018-09-22 18:29:24 -0400

    list slicing, copying, and reversing with slicing

--- 06/list_fun.py
+++ 06/list_fun.py
@@ -4,5 +4,8 @@ print(book_name)
 
 # The line below is an idiom for copying lists
 lotr = book_name[:]
+print(book_name[:3])
+print(book_name[3:])
+print(book_name[::-1])
 print(lotr is book_name)
 print(lotr == book_name)
