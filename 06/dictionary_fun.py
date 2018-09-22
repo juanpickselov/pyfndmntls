372bcc7c94eab30596d6540a70db6ae17a7d2e64
Juan Pickselov <juanpickselov@sbcglobal.net>
2018-09-22 19:53:23 -0400

    more collection  fun

---
+++ 06/dictionary_fun.py
@@ -0,0 +1,5 @@
+urls = {'github':'https://github.com/juanpickselov/',
+     'Pluralsight':'https://app.pluralsight.com/library/',
+     'Developerworks':'https://developer.ibm.com'}
+
+print(urls['github'])
