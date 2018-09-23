6b93e5ac7e69dc39b9bccb91fe5423f94f6cc5fe
Juan Pickselov <juanpickselov@sbcglobal.net>
2018-09-22 20:51:29 -0400

    more collection  fun

---
+++ 06/set_fun.py
@@ -0,0 +1,7 @@
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+alphabet_to_set = set(alphabet)
+phrase = 'the quick red fox jumps over the lazy brown dog'
+phrase_to_set = set(''.join(phrase.split()))
+print(phrase_to_set == alphabet_to_set)
+print(sorted(alphabet_to_set))
+print(sorted(phrase_to_set))
