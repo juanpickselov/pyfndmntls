5859e4a7f7133c24bc0ac2e2b8b467cdee00fc34
Juan Pickselov <juanpickselov@sbcglobal.net>
2018-09-22 20:13:19 -0400

    more collection  fun

--- 06/dictionary_fun.py
+++ 06/dictionary_fun.py
@@ -1,7 +1,12 @@
-urls = {'Github':'https://github.com/juanpickselov/',
-     'Pluralsight':'https://app.pluralsight.com/library/',
-     'Developerworks':'https://developer.ibm.com'}
+from collections import OrderedDict
 
-print(urls['Github'])
+urls = {'github':'https://github.com/juanpickselov/',
+    'Pluralsight':'https://app.pluralsight.com/library/',
+    'Developerworks':'https://developer.ibm.com'}
+
+sorted_urls = OrderedDict(sorted(urls.items()))
+print(urls['github'])
 # below is worth looking into further
-print(sorted(urls))
+# note the sorting appeas to be case sensitive
+print(sorted(urls, key=urls.__getitem__))
+print(sorted_urls)
