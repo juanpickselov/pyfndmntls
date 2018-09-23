c20213cae2d77aec341b84fdee7b2bce3285e1ee
Juan Pickselov <juanpickselov@sbcglobal.net>
2018-09-23 12:52:54 -0400

    more exception fun

--- 07/roots.py
+++ 07/roots.py
@@ -4,14 +4,15 @@ def sqrt(x):
         x: The number for which the square root is to be computed.
     Returns:
         The square root of x.
+    Raises:
+        ValueError: If x is negative
     '''
 
+    if x < 0:
+        raise ValueError('Cannot compute square root of negative number {}'.format(x))
     guess = x
     i = 0
-    try:
-        while guess * guess != x and i < 20:
-            guess = (guess + x / guess) / 2.0
-            i += 1
-    except ZeroDivisionError:
-        raise ValueError()
+    while guess * guess != x and i < 20:
+        guess = (guess + x / guess) / 2.0
+        i += 1
     return guess
